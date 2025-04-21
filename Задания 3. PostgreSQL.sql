76092fad821c269b54836090bed1077da7fd6c52
MattWay224 <mattew04@mail.ru>
2025-04-21 13:53:25 +0300

    task3 query3

--- Задания 3. PostgreSQL.sql
+++ Задания 3. PostgreSQL.sql
@@ -37,3 +37,18 @@ SELECT
         ORDER BY payment_date
         ) AS pred_amount
 FROM payment;
+
+/*
+ Задание 3. С помощью оконной функции определите,
+ на сколько каждый следующий платеж покупателя больше или меньше текущего.
+ */
+
+SELECT
+    customer_id,
+    amount,
+    lead(amount) OVER (
+        PARTITION BY customer_id
+        ORDER BY payment_date) - amount
+        AS difference   /* если нет знака - следующий платеж больше
+                       или равен, если присутствует -, то следующий платеж меньше текущего */
+FROM payment
